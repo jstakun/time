92752d006a9bed0520b404f2b8bf7fbbfde67977
root <root@ocp.local>
2020-12-07 07:27:00 -0500

    rhel7

--- pg-backup/do_backup.sh
+++ pg-backup/do_backup.sh
@@ -2,11 +2,11 @@
 cd /opt/backup
 export DUMP_FILE=pg-backup_`date +%Y%m%d_%H%M%S`.sql
 export PGPASSWORD=$POSTGRES_PASSWORD 
-echo 'pg_dump'
+echo '--- running pg_dump ---'
 pg_dump -v -d $POSTGRES_DB -U $POSTGRES_USER -h $POSTGRES_HOST --encoding=UTF-8 > $DUMP_FILE
-echo 'bzip2'
+echo '--- running bzip2 ---'
 bzip2 $DUMP_FILE
-echo 'mcrypt'
+echo '--- running mcrypt ---'
 mcrypt ${DUMP_FILE}.bz2 -k $DB_BACKUP_PASSWORD
-echo 'aws s3'
+echo '--- running aws s3 ---'
 aws s3 cp ${DUMP_FILE}.bz2.nc $S3_BACKUP_PATH/$DUMP_FILE.bz2.nc
